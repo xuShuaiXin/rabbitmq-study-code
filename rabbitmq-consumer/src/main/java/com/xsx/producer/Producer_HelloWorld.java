e3e886a19c099ecb774618384921d38cd848e861
xuShuaiXin <xu196404852@gmail.com>
2021-05-03 21:55:43 +0800

    RabbitMQ 简单模式入门案例

---
+++ rabbitmq-consumer/src/main/java/com/xsx/producer/Producer_HelloWorld.java
@@ -0,0 +1,59 @@
+package com.xsx.producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+//RabbitMQ 生产者发送消息
+public class Producer_HelloWorld {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //1.创建连接工厂
+        ConnectionFactory factory =  new ConnectionFactory();
+        //2.设置参数
+        factory.setHost("192.168.43.197");  //ip  默认值 localhost
+        factory.setPort(5672);              //端口  默认值 5672
+        factory.setVirtualHost("/xsx");     //虚拟机 默认值/
+        factory.setUsername("xsx");         //用户名 默认 guest
+        factory.setPassword("xsx123456");   //密码 默认值 guest
+        //3.从工厂类中获取连接
+        Connection connection = factory.newConnection();
+        //4.创建channel
+        Channel channel = connection.createChannel();
+        //5.创建队列 Queue
+        /**
+         * queueDeclare(String queue, boolean durable,
+         *              boolean exclusive, boolean autoDelete,
+         *              Map<String, Object>
+         *    参数:
+         *      1.queue:        队列名称
+         *      2.durable:      是否持久化,如果为true会保存到硬盘上,mq重启后也还在
+         *      3.exclusive:
+         *          * 是否独占。只能有一个消费者监听这队列
+         *          * 当Connection关闭时，是否删除队列
+         *      4.autoDelete:   是否自动删除
+         *      5.arguments:    参数
+         */
+        //如果没有一个名字叫hello_world的队列，则会创建该队列，如果有则不会创建
+        channel.queueDeclare("hello_world", true, false, false,null);
+
+        //6.发送消息
+        /**
+         * basicPublish(String exchange, String routingKey, BasicProperties props, byte[] body
+         *    参数:
+         *      1. exchange：交换机名称。简单模式下交换机会使用默认的 ""
+         *      2. routingKey：路由名称
+         *      3. props：配置信息
+         *      4. body：发送消息数据
+         */
+        String body = "hello RabbitMQ!";
+        channel.basicPublish("",  "hello_world", null, body.getBytes());
+
+//        //7.关闭资源
+        channel.close();
+        connection.close();
+
+    }
+}
